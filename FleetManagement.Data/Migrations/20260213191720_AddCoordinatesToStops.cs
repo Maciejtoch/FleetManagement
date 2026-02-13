99d32175375f7c996aa72ac70de86337e7849152
Maciej T <tochowicz.maciej234@gmail.com>
2026-02-13 20:32:59 +0100

    Change of UI, MAP API work in progress, added functionality to stops tab

---
+++ FleetManagement.Data/Migrations/20260213191720_AddCoordinatesToStops.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoordinatesToStops : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Stops",
+                type: "REAL",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Stops",
+                type: "REAL",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Stops");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Stops");
+        }
+    }
+}
