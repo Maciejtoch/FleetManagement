aaa30766d287bf5700e6a828f18f9c7118249693
Maciej T <tochowicz.maciej234@gmail.com>
2026-02-13 20:15:45 +0100

    Change of UI, MAP API work in progress, added functionality to stops tab

---
+++ FleetManagement.Data/Migrations/20260213185839_StopsFunctionalityAddOn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StopsFunctionalityAddOn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnMap",
+                table: "Stops",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnMap",
+                table: "Stops");
+        }
+    }
+}
